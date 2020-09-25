53a64b5fd108af27f4a84813019d004d3b62c451
Nick Young <60262965+nickyounguncg@users.noreply.github.com>
2020-09-25 09:05:11 -0400

    Update get-project-list.sh

--- get-project-list.sh
+++ get-project-list.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-gcloud projects list --format="csv(createTime.date('%Y-%m-%d'),name,projectNumber,projectId)"
+# Usage to get projects from one ORG ID: ./get-project-list.sh 123456789
+# Usage to get projects from all (that you have access to): ./get-project-list.sh
+# Results: CSV containing Create Time, Name, Project Number, Project ID, Life Cycle State
+# Project fields that can be returned are documented here: https://cloud.google.com/sdk/gcloud/reference/projects/describe
+#
+# if the first argument is not passed in, then assume that we want to grab ALL projects, not just from a particular ORG ID
+if [ $# -eq 1 ]
+then
+  # if the first argument IS passed in, then only return projects from that ORG ID
+  gcloud projects list --filter "parent.type=organization parent.id=$1" --format="csv(createTime.date('%Y-%m-%d'),name,projectNumber,projectId,lifecycleState)"
+else
+	gcloud projects list --format="csv(createTime.date('%Y-%m-%d'),name,projectNumber,projectId,lifecycleState)"
+fi
